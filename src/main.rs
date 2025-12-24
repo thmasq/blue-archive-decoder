4d3320aa9413a43c15b6d7709377ca88f553d01b
Thomas Q <thomasqsa@gmail.com>
2025-12-24 19:41:53 -0300

    Add flatbuffer generated decoding

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,7 @@
-use clap::Parser;
-use rusqlite::Connection;
-use std::error::Error;
-use std::path::Path;
+extern crate alloc;
 
-mod localize_excel;
-mod localize_excel_generated;
+mod blue_archive_generated;
 
-use localize_excel::process_localize_db_schema;
-
-#[derive(Parser)]
-#[command(name = "localize-db-modifier")]
-#[command(
-    about = "Modifies Blue Archive localization database by decoding FlatBuffer blobs into language columns"
-)]
-struct Args {
-    database: String,
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-
-    if !Path::new(&args.database).exists() {
-        eprintln!("Error: Database file '{}' does not exist", args.database);
-        std::process::exit(1);
-    }
-
-    println!("Processing database: {}", args.database);
-
-    let mut conn = Connection::open(&args.database)?;
-
-    // Process LocalizeDBSchema table
-    process_localize_db_schema(&mut conn)?;
-
-    println!("Database modification complete!");
-
-    Ok(())
+fn main() {
+    println!("Hello world!")
 }
